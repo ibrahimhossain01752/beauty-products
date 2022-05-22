0b76c34e20385f4f54ba2b74b67c75fbac606115
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-05-23 05:44:49 +0600

    payments added

---
+++ src/Pages/Dashboard/Dashboard/Payments/Payments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Payments = () => {
+    return (
+        <div class="mt-5 pt-5">
+            <h2>
+             Payments system will be added.....Plz stay connected.
+            </h2>
+        </div>
+    );
+};
+
+export default Payments;
