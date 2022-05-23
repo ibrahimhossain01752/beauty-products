9c90d8f87d1be236ce804cba6d4c5548f375e54a
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-05-23 06:19:16 +0600

    admin

---
+++ src/Pages/Dashboard/Dashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const MakeAdmin = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        fetch("http://localhost:5000/dashboard/makeAdmin", {
+          method: "PUT",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(data),
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            if(result.matchedCount){
+              alert("Admin Create SuccessFully")
+            }else{
+              alert("User Not Found")
+            }
+          });
+      };
+      
+    
+    
+    return (
+        <div>
+      <h1>make admin</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="input-field"
+          name="email"
+          placeholder="Email"
+          type="email"
+          {...register("email", { required: true })}
+        />
+        <br />
+
+        <input
+          className="submit-btn btn btn-danger mt-3"
+          type="submit"
+          value="make as Admin"
+        />
+      </form>
+    </div>
+    );
+};
+
+export default MakeAdmin;
