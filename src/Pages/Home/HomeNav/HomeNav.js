e66a767edb29d2a9cd15cd7595b3c4e14c2b3bcd
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-05-31 20:26:43 +0600

    navhead added

---
+++ src/Pages/Home/HomeNav/HomeNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeNav = () => {
+    return (
+        <div>
+            <img class="home-nav" src="https://cdn.shopify.com/s/files/1/0195/5145/3248/t/2/assets/logo.png?v=22716499900373348481572590512" alt="" />
+            <hr class="home-hr"/>
+        </div>
+    );
+};
+
+export default HomeNav;
--- src/Pages/Home/Shared/Header/Header.js
+++ src/Pages/Home/Shared/Header/Header.js
@@ -4,30 +4,33 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../../hooks/useAuth';
+import HomeNav from '../../HomeNav/HomeNav';
 
 
 const Header = () => {
     const { user, logout} = useAuth();
     return (
+        
         <>
-           <Navbar className="bg" variant="dark" sticky="top" collapseOnSelect expand="lg">
+        <HomeNav></HomeNav>
+           <Navbar className="bg"  sticky="top" collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">Travel</Navbar.Brand>
+                    <Navbar.Brand className="nav-head" href="#home">Her Pride</Navbar.Brand>
                     <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={HashLink} className="text-light" to="/home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} className="text-light" to="/Collection">Colllection</Nav.Link>
-                         <Nav.Link as={HashLink} className="text-light" to="/blogs">Blogs</Nav.Link>
+                    <Navbar.Collapse className="justify-content-center headerst">
+                        <Nav.Link as={HashLink} className=" headerst" to="/home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} className=" headerst" to="/Collection">Colllection</Nav.Link>
+                         <Nav.Link as={HashLink} className=" headerst" to="/blogs">Blogs</Nav.Link>
                         {/* {user?.email && <Nav.Link as={HashLink} className="text-light" to="/collections">Colllection</Nav.Link>}
                         {user?.email && <Nav.Link as={HashLink} className="text-light" to="/blogs">Blogs</Nav.Link>} */}
-                        {user?.email && <Nav.Link as={HashLink} className="text-light" to="/login">Login</Nav.Link>}
+                        {user?.email && <Nav.Link as={HashLink} className=" headerst" to="/login">Login</Nav.Link>}
                         <Navbar.Text><a href="#login" className="text-decoration-none p-2 text-warning"> {user?.displayName} </a> </Navbar.Text>
                         {user?.email ?
                             <Button className="btn11" onClick={logout} variant="primary">logOut</Button>
                             :
                             <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         }
-                        {user?.email && <Nav.Link as={HashLink} className="text-light" to="/dashboard">Dashboard</Nav.Link>}
+                        {user?.email && <Nav.Link as={HashLink} className=" headerst" to="/dashboard">Dashboard</Nav.Link>}
 
                         {/* <Navbar.Text><a href="#login" className="text-decoration-none ">  {user?.displayName}</a> </Navbar.Text> */}
                     </Navbar.Collapse>
